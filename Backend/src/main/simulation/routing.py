fb9564febf94ffe3d79e00b43e1ff6690942d85a
lukas.ling <lukas.ling@epita.fr>
2025-07-24 13:48:25 +0200

    fix: backend archi

--- Backend/src/main/simulation/routing.py
+++
@@ -1,6 +0,0 @@
-from django.urls import re_path
-from . import consumers
-
-websocket_urlpatterns = [
-    re_path(r'ws/simulation/(?P<simulation_id>\w+)/$', consumers.SimulationConsumer.as_asgi()),
-]
